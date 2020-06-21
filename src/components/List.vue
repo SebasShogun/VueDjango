ed7fcb2567919ce42800f9e1c7adbaab51a5c74a
SebasShogun <shogun31sff@gmail.com>
2020-06-20 23:55:48 -0500

    Inicio de Vue y ciclos Vue

---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+
+        <div class="box" v-for="e in elements" v-bind:key="e">
+            {{ e }}
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            elements:["Uno", "Dos", "Tres"]
+        };
+    }
+}
+</script>
+
+<style>
+    .box{
+        border: 3px solid #CCC;
+        margin: 8px 0 0 0;
+    }
+</style>
