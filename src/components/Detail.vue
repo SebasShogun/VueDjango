ff264e4f97819bd614c098a223a306ae37488809
SebasShogun <shogun31sff@gmail.com>
2020-06-21 04:23:18 -0500

    Mejora de vista de carta, Detail vista y Detail peticiones

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,31 +1,56 @@
 <template>
-    <div class="container">
-        <b-card :title="element.title">
+    <div>
+        <b-card>
+        <h1>{{ element.title }}</h1>
+        <div class="p-3">
+            <router-link class="btn btn-danger btn-sm" 
+            :to="{ name:'list-category',params:{id: element.id} }">{{ category.title }}</router-link>
+            <router-link class="ml-2 btn btn-danger btn-sm"
+            :to="{ name:'list-type',params:{id: element.id} }">{{ type.title }}</router-link>
             <b-card-text>{{ element.description }}</b-card-text>
+        </div>
         </b-card>
     </div>
 </template>
 
 <script>
 export default {
-
-    created(){
-        this.find();
+  created() {
+    this.find();
+  },
+  data() {
+    return {
+      element: Object,
+      category: Object,
+      type: Object
+    };
+  },
+  methods: {
+    find: function() {
+      fetch(
+        "http://localhost:8000/api/element/" +
+          this.$route.params.id +
+          "/?format=json"
+      )
+        .then(res => res.json())
+        .then(res => {
+          this.element = res;
+          this.findCategory(this.element.id);
+          this.findType(this.element.id);
+        });
     },
-
-    data(){
-        return {
-            element: Object
-        };
-    }, 
-    methods: {
-        find: function(){
-            fetch("http://127.0.0.1:8000/api/element/"+this.$route.params.id+"/?format=json")
-                .then(res => res.json())
-                .then(res => this.element = res)
-        }
+    findCategory: function(id) {
+      axios.get("http://localhost:8000/api/category/" + id + "/?format=json")
+        //.then(res => res.json())
+        .then(res => (this.category = res.data));
     },
-}
+    findType: function(id) {
+      axios.get("http://localhost:8000/api/type/" + id + "/?format=json")
+        //.then(res => res.json())
+        .then(res => (this.type = res.data));
+    }
+  }
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -20,8 +20,8 @@ Vue.config.productionTip = false
 const routes = [
   { path: '/', component: List },
   { path: '/detail/:id', component: Detail },
-  { path: '/category/:id/elements', component: ListCategory },
-  { path: '/type/:id/elements', component: ListType }
+  { path: '/category/:id/elements', component: ListCategory, name: 'list-category' },
+  { path: '/type/:id/elements', component: ListType, name: 'list-type' }
 ]
 
 const router = new VueRouter({
